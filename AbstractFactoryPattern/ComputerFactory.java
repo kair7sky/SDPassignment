12dccf06f66312f91e4b5e9bd66b6350c1002bbe
Abylaikhan <118976474+kair7sky@users.noreply.github.com>
2023-10-10 18:16:43 +0600

    SDP Abstract
    ASSIK-1

---
+++ AbstractFactoryPattern/ComputerFactory.java
@@ -0,0 +1,7 @@
+package AbstractFactoryPattern;
+
+public class ComputerFactory {
+    public static Computer getComputer(ComputerAbstractFactory factory){
+        return factory.createComputer();
+    }
+}
